240a3c03b23f196e29ecd339e0aa22474eb1c9e7
Dev Patel <109199551+ggpop00@users.noreply.github.com>
2023-06-27 22:01:20 +0530

    Add files via upload

---
+++ Assignment4.py
@@ -0,0 +1,53 @@
+#for with else loop
+# for i in "Dev":
+#     if i=="c":
+#         break
+#     else:
+#         print(i)
+# else:
+#     print("This has break")
+
+# for i in "Dev":
+#     if i=="n":
+#         pass
+#     else:
+#         print(i)
+# else:
+#     print("This has pass")
+
+
+# for i in "Dev":
+#     if i=="r":
+#         continue
+#     else:
+#         print(i)
+# else:
+#     print("This has continue")
+
+#while with else loop
+# i=1
+# while i<=10:
+#     if i==5:
+#         break
+#     else:
+#         print(i)
+# else:
+#     print("This has break")
+
+# i=1
+# while i<=10:
+#     if i==5:
+#         continue
+#     else:
+#         print(i)
+# else:
+#     print("This has continue")
+
+# i=1
+# while i<=10:
+#     if i==5:
+#         pass
+#     else:
+#         print(i)
+# else:
+#     print("This has pass")
